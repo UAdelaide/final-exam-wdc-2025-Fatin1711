b93960ac168ea23f3d24b82d84f0b79a50757ddc
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 14:42:04 +0000

    [automatic commit] 2025-06-20 14:42:04 UTC 125.168.137.50

--- part1/app.js
+++ part1/app.js
@@ -122,8 +122,8 @@ let db;
 
       await db.execute(`
         INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
-((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
-((SELECT dog_id FROM Dogs WHERE name = 'Bella' AND owner_id = (SELECT user_id FROM Users WHERE username = 'carol123')),
+        ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+        ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), AND owner_id = (SELECT user_id FROM Users WHERE username = 'carol123')),
 '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
 ((SELECT dog_id FROM Dogs WHERE name = 'Ben' AND owner_id = (SELECT user_id FROM Users WHERE username = 'samrudge')),
 '2025-06-20 14:30:00', 60, 'Adelaide', 'open'),
