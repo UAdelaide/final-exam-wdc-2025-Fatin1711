8dd4b7ff5d4882ba77e79e965c977f19d081db3d
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 14:50:37 +0000

    [automatic commit] 2025-06-20 14:50:37 UTC 125.168.137.50

--- part1/app.js
+++ part1/app.js
@@ -131,7 +131,7 @@ let db;
 
       await db.execute(`
         INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments) VALUES
-        (2, (SELECT user_id FROM Users WHERE username = 'carol123'), 5, 'Great j')
+        (2, (SELECT user_id FROM Users WHERE username = 'carol123'), 5, 'Great job!'),
         ('tommartindal', 'tom@yahoo.com', 'hashed111', 'walker')
       `);
     }
