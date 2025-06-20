31a187b5856f849a2853499905f9faeddb58662d
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 14:33:09 +0000

    [automatic commit] 2025-06-20 14:33:09 UTC 125.168.137.50

--- part1/app.js
+++ part1/app.js
@@ -101,9 +101,9 @@ let db;
 
     // Insert data if table is empty
     const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
-    if (rows[0].count === 0) {
+    if (users[0].count === 0) {
       await db.execute(`
-        INSERT INTO books (title, author) VALUES
+        INSERT INTO Users (username, author) VALUES
         ('1984', 'George Orwell'),
         ('To Kill a Mockingbird', 'Harper Lee'),
         ('Brave New World', 'Aldous Huxley')
