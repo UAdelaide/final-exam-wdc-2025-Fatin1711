b402aad85800014044a8ac33f218811d34637a29
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 14:35:54 +0000

    [automatic commit] 2025-06-20 14:35:54 UTC 125.168.137.50

--- part1/app.js
+++ part1/app.js
@@ -119,6 +119,24 @@ let db;
         ('samrudge', 'sam_rudge@gmail,com', 'hashed000', 'owner'),
         ('tommartindal', 'tom@yahoo.com', 'hashed111', 'walker')
       `);
+
+      await db.execute(`
+        INSERT INTO Users (username, email, password_hash, role) VALUES
+        ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+        ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+        ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+        ('samrudge', 'sam_rudge@gmail,com', 'hashed000', 'owner'),
+        ('tommartindal', 'tom@yahoo.com', 'hashed111', 'walker')
+      `);
+
+      await db.execute(`
+        INSERT INTO Users (username, email, password_hash, role) VALUES
+        ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+        ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+        ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+        ('samrudge', 'sam_rudge@gmail,com', 'hashed000', 'owner'),
+        ('tommartindal', 'tom@yahoo.com', 'hashed111', 'walker')
+      `);
     }
   } catch (err) {
     console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
