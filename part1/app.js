e283e11fc4140afa3ec97e713c614033f5748196
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 14:54:22 +0000

    [automatic commit] 2025-06-20 14:54:22 UTC 125.168.137.50

--- part1/app.js
+++ part1/app.js
@@ -135,7 +135,7 @@ let db;
         (3, (SELECT user_id FROM Users WHERE username = 'tommartindal'), (SELECT user_id FROM Users WHERE username = 'samrudge'), 4, 'Well done'),
       `);
 
-      await db.execute(`UPDATE WalkRequests SET status = 'completed` WHERE req)
+      await db.execute(`UPDATE WalkRequests SET status = 'completed` WHERE request_id IN (2, 3))
     }
   } catch (err) {
     console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
