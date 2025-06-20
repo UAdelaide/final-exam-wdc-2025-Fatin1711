66587aa20845691127ef15ba55d4936d39335e91
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 13:41:11 +0000

    [automatic commit] 2025-06-20 13:41:11 UTC 125.168.137.50

--- part1/part5.sql
+++ part1/part5.sql
@@ -17,4 +17,4 @@ INSERT INTO Dogs (owner_id, name, size) VALUES
 
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
-((SELECT dog_id FROM Dogs WHERE name = 'Max' AND owner_id = ()))
+((SELECT dog_id FROM Dogs WHERE name = 'Max' AND owner_id = (SELECT user_id FROM Users WHERE u)))
